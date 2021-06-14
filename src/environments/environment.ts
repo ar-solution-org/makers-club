5ddf2f449cd1d1e97dab8a07721a3127380a5908
Aiko Radlingmayr <radlingmayr.a@gmail.com>
2021-06-14 21:04:30 +0200

    add firebase project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBJQ6sk1gVjL8AbTYYc14nfhvIcrQD1WmU",
+        authDomain: "makers-club-c93bd.firebaseapp.com",
+        projectId: "makers-club-c93bd",
+        storageBucket: "makers-club-c93bd.appspot.com",
+        messagingSenderId: "72679871001",
+        appId: "1:72679871001:web:3c375c47d5f48f773dd088",
+        measurementId: "G-ZDSQ23KVP3",
+    },
 };
 
 /*
